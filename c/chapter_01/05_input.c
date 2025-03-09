e87065d64dee23ad514cdbd7dfe843248f473b7b
unknown <mahsaanabbasi@gmail.com>
2025-03-10 03:44:56 +0500

    Update input.c to calculate the area of a rectangle instead of a single integer input

--- c/chapter_01/05_input.c
+++ c/chapter_01/05_input.c
@@ -1,8 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-    int a; 
-    scanf("%d", &a);
-    printf("The value of a is %d", a);
+    // int length = 3;
+    // int breadth = 6;
+    int length, breadth;
+    printf("Enter length\n");
+    scanf("%d", &length);
+
+    printf("Enter breadth\n");
+    scanf("%d", &breadth);
+
+    printf("The area of this rectangle is %d", length*breadth);
     return 0;
 }

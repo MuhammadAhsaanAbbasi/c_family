abf4d8002cf2df20a56d49a5d2e8265d5e862bb7
unknown <mahsaanabbasi@gmail.com>
2025-03-10 15:08:00 +0500

    Update input.c to use hardcoded values for rectangle area calculation and add new statement.c example

--- c/chapter_01/05_input.c
+++ c/chapter_01/05_input.c
@@ -4,11 +4,13 @@ int main(){
     // int length = 3;
     // int breadth = 6;
     int length, breadth;
+    length = 3;
+    breadth=6;
     printf("Enter length\n");
-    scanf("%d", &length);
+    // scanf("%d", &length);
 
     printf("Enter breadth\n");
-    scanf("%d", &breadth);
+    // scanf("%d", &breadth);
 
     printf("The area of this rectangle is %d", length*breadth);
     return 0;

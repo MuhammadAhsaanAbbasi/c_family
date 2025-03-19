3b25969da9117fec29a2cc1ddeb780f75abc855f
unknown <mahsaanabbasi@gmail.com>
2025-03-20 04:38:13 +0500

    Implement odd/even number counting game logic in class03.c

--- c/classwork/class03.c
+++ c/classwork/class03.c
@@ -5,6 +5,52 @@
 #include<stdio.h>
 
 void main(){
-
-    printf('Hello World');
+    int even_number, odd_number;
+    char continue_game;
+    do
+    {
+        even_number=0;
+        odd_number=0;
+        for (int i = 1; i <= 5; i++)
+        {
+            int num;
+            printf("Enter the number: ");
+            scanf("%d", &num);
+            if(num % 2 == 0) {
+                even_number++;
+            } else if (num % 2 != 0)
+            {
+                odd_number++;
+            }
+        };
+        if(even_number > odd_number) {
+            printf("Even Numbers: %d\n", even_number);
+            printf("Odd Numbers: %d\n", odd_number);
+            printf("Even numbers are greater than odd numbers. Restart the game.\n");
+        } else if (even_number == odd_number)
+        {
+            printf("Even Numbers: %d\n", even_number);
+            printf("Odd Numbers: %d\n", odd_number);
+            printf("Even numbers are equal to odd numbers. Do you want to continue? (Y/N): ");
+            scanf(" %c", &continue_game);
+            if(continue_game == 'Y' || continue_game == 'y') {
+                continue;
+            } else if (continue_game == 'N' || continue_game == 'n')
+            {
+                break;
+            }
+        } else
+        {
+            printf("Even Numbers: %d\n", even_number);
+            printf("Odd Numbers: %d\n", odd_number);
+            printf("Odd numbers are greater than even numbers. Do you want to continue? (Y/N): ");
+            scanf(" %c", &continue_game);
+            if(continue_game == 'Y' || continue_game == 'y') {
+                continue;
+            } else if (continue_game == 'N' || continue_game == 'n')
+            {
+                break;
+            }
+        }
+    } while (1);
 }

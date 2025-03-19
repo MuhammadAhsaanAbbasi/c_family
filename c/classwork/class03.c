072dd4c3728229c4a57a58cda271a6493c0cc1d7
unknown <mahsaanabbasi@gmail.com>
2025-03-19 17:33:15 +0500

    Add class03.c for odd/even input game logic implementation

---
+++ c/classwork/class03.c
@@ -0,0 +1,10 @@
+// Write a program that takes 5 inputs in a loop & calculate the number of inputs are odd and even.
+// If Number of even is greater than odd then restart the game, If game is tied so show the message
+// are you continued or not, If continue so restart the game otherwise exit the game.
+
+#include<stdio.h>
+
+void main(){
+
+    printf('Hello World');
+}

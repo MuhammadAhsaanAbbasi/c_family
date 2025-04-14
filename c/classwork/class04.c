99c867ee0f8964144822b617aec1a18e31368b48
unknown <mahsaanabbasi@gmail.com>
2025-04-14 07:03:33 +0500

    Add calculator and random number guessing game implementations

--- c/classwork/class04.c
+++ c/classwork/class04.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+#include <conio.h>
 
 void main(){
-
-    printf('Hello World');
+    int a;
+    for (a=1; a<=10; a++){
+        printf("%d\n", a*a*a);
+        if (a==5){
+            break;
+        }
+    }
 }
